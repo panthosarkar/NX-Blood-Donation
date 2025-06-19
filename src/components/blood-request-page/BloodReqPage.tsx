0403429ee11a294654fc8b60d202b84ddaaed2bd
Pantho Sarkar <pantho.sarkar.7@gmail.com>
2025-06-19 11:44:50 +0600

    feat: update BloodReqPage content and styling for improved user guidance

--- src/components/blood-request-page/BloodReqPage.tsx
+++ src/components/blood-request-page/BloodReqPage.tsx
@@ -3,11 +3,15 @@ import React from "react";
 const BloodReqPage = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold text-center my-8">
-        Blood Request Page
+      <h1 className="text-[45px] leading-[55px] font-bold text-start my-8">
+        Request Information
       </h1>
-      <p className="text-center text-gray-600">
-        This is the blood request page where users can request blood donations.
+      <p className="text-start text-gray w-[830px] text-lg leading-5">
+        Fill out the form to request blood for yourself or someone in need.
+        Provide key details like blood type, number of units, hospital, and
+        urgency. Our system will instantly alert nearby verified donors. Every
+        request is handled with speed, care, and confidentiality. Together, we
+        can help save lives—one donation at a time.{" "}
       </p>
       {/* Additional content and components can be added here */}
     </div>
